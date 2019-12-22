238710d03cd22d164dfb540eeae3e489bfebee47
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 13:59:06 +0200

    Add files via upload

---
+++ ArraysExercise/MagicSum/MagicSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNum1 = numbers[i];
+
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    int currentNum2 = numbers[j];
+                    if (currentNum1 + currentNum2 == num)
+                    {
+                        Console.WriteLine($"{currentNum1} {currentNum2}");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ ArraysExercise/TopIntegers/TopIntegers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int currentNum = numbers[i];
+                bool isTop = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int otherNumber = numbers[j];
+
+                    if (currentNum > otherNumber)
+                    {
+                        Console.Write(currentNum + " ");
+                        break;
+                    }
+                    else
+                    {
+                        isTop = false;
+                        break;
+                    }
+
+                }
+            }
+            Console.WriteLine(numbers[numbers.Length-1]);
+        }
+    }
+}
