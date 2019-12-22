80d4ed19779b4db50f28b18f8309d31dc350967f
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 13:28:49 +0200

    Add files via upload

---
+++ BasicSyntaxExercise/RageExpence/RageExpence.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RageExpence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displeyPrice = double.Parse(Console.ReadLine());
+
+            int trashedHeadsetCount = 0;
+            int trashedMouseCount = 0;
+            int trashedKeyboardCount = 0;
+            int trashedDispleyCount = 0;
+
+            for(int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    trashedHeadsetCount++;
+                }
+                if (i % 3 == 0)
+                {
+                    trashedMouseCount++;
+                }
+                if (i % 6 == 0)
+                {
+                    
+                    trashedKeyboardCount++;
+                }
+                if (i%12==0)
+                {
+                    trashedDispleyCount++;
+                }
+            }
+            double totalSum = (headsetPrice * trashedHeadsetCount) + (mousePrice * trashedMouseCount) + (keyboardPrice * trashedKeyboardCount) + (displeyPrice * trashedDispleyCount);
+            Console.WriteLine($"Rage expenses: {totalSum:f2} lv.");
+
+        }
+    }
+}
---
+++ BasicSyntaxExercise/Vacation/Vacation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            string nameOfGroup = Console.ReadLine();
+            string dayOfTheWeek = Console.ReadLine();
+
+            double price = 0;
+            double discount = 0.0;
+           
+            if (nameOfGroup == "Students")
+            {
+                if (dayOfTheWeek == "Friday")
+                {
+                    price = 8.45;
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    price = 9.80;
+                }
+                else if (dayOfTheWeek == "Sunday")
+                {
+                    price = 10.46;
+                }
+                if (numberOfPeople >= 30)
+                {
+                    discount = price * 0.15;
+                    price -= discount;
+                }
+                
+            }
+            if (nameOfGroup == "Business")
+            {
+                if (dayOfTheWeek == "Friday")
+                {
+                    price = 10.90;
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    price = 15.60;
+                }
+                else if (dayOfTheWeek == "Sunday")
+                {
+                    price = 16;
+                }
+                if (numberOfPeople >= 100)
+                {
+                    numberOfPeople -= 10;
+                }
+                
+            }
+            if (nameOfGroup == "Regular")
+            {
+                if (dayOfTheWeek == "Friday")
+                {
+                    price =15;
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    price = 20;
+                }
+                else if (dayOfTheWeek == "Sunday")
+                {
+                    price = 22.50;
+                }
+                if (numberOfPeople >= 10 && numberOfPeople<=20)
+                {
+                    discount = price / 0.05;
+                    price -= discount;
+                }
+            }
+            double sum = numberOfPeople * price;
+            Console.WriteLine($"Total price: {sum:f2}");
+        }
+    }
+}
