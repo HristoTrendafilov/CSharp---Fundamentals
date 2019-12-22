43dfd94ff39274473dc2a1c7521289cc6ca4fb9a
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 14:02:27 +0200

    Add files via upload

---
+++ ArraysExercise/DayofWeek/DayofWeek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DayofWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = int.Parse(Console.ReadLine());
+            string[] day = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",               
+            "Saturday",                 
+            "Sunday"};
+
+            if(dayOfWeek>=1 && dayOfWeek <= 7)
+            {
+                Console.WriteLine(day[dayOfWeek - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    
+    }
+}
---
+++ ArraysExercise/EqualArrays/EqualArrays .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+namespace EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbers2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            bool isFalse = false;
+
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                sum += numbers1[i];
+                if (numbers1[i] != numbers2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isFalse = true;
+                    break;
+                }
+                
+            }
+            if (!isFalse)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+            
+            
+
+        }
+    }
+}
