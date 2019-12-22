ee85a808e9eaf1a534158d9162a94be449c04168
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 14:00:10 +0200

    Add files via upload

---
+++ ArraysExercise/Train/Train.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] peopleOnTheTrain = new int[count];
+            int sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                int newPassagers = int.Parse(Console.ReadLine());
+                sum += newPassagers;
+                peopleOnTheTrain[i] = newPassagers;
+                
+            }
+            for (int i = 0; i < peopleOnTheTrain.Length; i++)
+            {
+                Console.Write($"{peopleOnTheTrain[i]} ");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ ArraysExercise/Zig-ZagArrays/Zig-ZagArrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+namespace Zig_ZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int[] first = new int[count];
+            int[] second = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (i % 2 == 0)
+                {
+                    first[i] = numbers[0];
+                    second[i] = numbers[1];
+                }
+                else
+                {
+                    first[i] = numbers[1];
+                    second[i] = numbers[0];
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", first));
+            Console.WriteLine(string.Join(" ", second));
+           
+
+
+              
+            
+        }
+    }
+}
