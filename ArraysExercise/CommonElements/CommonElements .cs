39cb2c9755538ac0b3736f02f4aa6ae1e4771cfe
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 13:53:53 +0200

    Add files via upload

---
+++ ArraysExercise/CommonElements/CommonElements .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str1 = Console.ReadLine().Split();
+            string[] str2 = Console.ReadLine().Split();
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                for (int j = 0; j < str1.Length; j++)
+                {
+                    if (str2[i] == str1[j])
+                    {
+                        Console.Write($"{str2[i]} ");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ ArraysExercise/Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dnaSequences = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+            int sum = 0;
+            
+            while(command != "Clone them!")
+            {
+                string[] dnaSequence = Console.ReadLine().Split("!").ToArray();
+                if(dnaSequence="Clone Them!")
+                {
+
+                }
+                int[] dnaSequenceInt = Array.ConvertAll(dnaSequence, Int32.Parse);
+
+                for (int i = 0; i < dnaSequenceInt.Length; i++)
+                {
+                    sum += dnaSequenceInt[i];
+                    
+                }
+            }
+            Console.WriteLine("pffff");
+        }   
+    }
+}
