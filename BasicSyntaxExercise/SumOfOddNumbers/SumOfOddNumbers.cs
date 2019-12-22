b5a7ab940d622b08694a11b0cc7be8c0ec1fece0
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 13:51:52 +0200

    Add files via upload

---
+++ BasicSyntaxExercise/SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int loop = 0;
+            int oddNumber = 1;
+
+            while (loop != num)
+            {
+                Console.WriteLine(oddNumber);
+                sum += oddNumber;
+                oddNumber += 2;
+                    loop++;
+               
+            }
+
+           
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ BasicSyntaxExercise/Theatre Promotion/Theatre Promotion.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Theatre_Promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weekDay = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int price = 0;
+
+            if (weekDay == "Weekday")
+            {
+                if(age>=0 && age <= 18)
+                {
+                    price = 12;
+                }
+                else if(age>18 && age <= 64)
+                {
+                    price = 18;
+                }
+                else if(age>64 && age <= 122)
+                {
+                    price = 12;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+            if (weekDay == "Weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+            if (weekDay == "Holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+            if (age >= 0)
+            {
+                Console.WriteLine($"{price}$");
+            }
+        }
+    }
+}
