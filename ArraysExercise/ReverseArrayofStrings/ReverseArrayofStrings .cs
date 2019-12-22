43ad3f93853ed71a9d7098aa2b6ac79e3ef323b3
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2019-12-22 14:04:34 +0200

    Add files via upload

---
+++ ArraysExercise/ReverseArrayofStrings/ReverseArrayofStrings .cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseArrayofStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            string[] revStr = new string[str.Length];
+
+            for (int i = str.Length-1;i>=0;i--)
+            {
+                revStr[i] = str[i];
+                Console.Write($"{revStr[i]} ");
+            }
+        }
+    }
+}
