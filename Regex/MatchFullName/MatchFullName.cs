e0aee82066e4fed253cccc022d9ad36d129db91f
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-10-24 18:25:22 +0300

    Add files via upload

---
+++ Regex/MatchFullName/MatchFullName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine();
+
+            var regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+");
+
+            var matches = regex.Matches(names);
+
+            foreach (Match match in matches)
+            {
+                Console.Write($"{match} ");
+            }
+        }
+    }
+}
---
+++ Regex/MatchPhoneNumber/MatchPhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phoneNumbers =Console.ReadLine();
+
+            var regex = new Regex(@"[+][3][5][9](-| )[2]\1\d{3}\1\d{4}\b");
+
+            MatchCollection matches = regex.Matches(phoneNumbers);
+
+            Console.WriteLine(string.Join(", ",matches));
+        }
+    }
+}
