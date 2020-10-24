bda347592b56458fdb146143daeb4c63efae68a1
Hristo Trendafilov <hristo.trendafilov93@gmail.com>
2020-10-24 18:12:24 +0300

    Add files via upload

---
+++ ArraysLab/CondenseArraytoNumber/CondenseArraytoNumber .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace CondenseArraytoNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                int[] condenced = new int[numbers.Length];
+                condenced[i] = numbers[i] + numbers[i + 1];
+                
+                            
+                Console.WriteLine(condenced[i]);
+                
+            }
+            Console.WriteLine(numbers[0]);
+
+        }
+    }
+}
